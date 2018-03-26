f7c3d8093f5a053a7a5c64522468d649f29ab50a
afdhal <afdhal.hr@gmail.com>
2018-03-26 16:21:56 +0700

    Fix:Django Level Three done

--- ProTwo/appTwo/views.py
+++ ProTwo/appTwo/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from appTwo.models import User
+from appTwo.forms import NewUserForm
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<em>My Second App!</em>")
+	# return HttpResponse("<em>My Second App!</em>")
+	return render(request, 'appTwo/index.html')
 
 def help(request):
 	helpdict = {'help_insert': 'HELP PAGE'}
 	return render(request, 'appTwo/help.html', context=helpdict)
 
+# def users(request):
+# 	user_list = User.objects.order_by('first_name')
+# 	user_dict = {'users':user_list}
+# 	return render(request, 'appTwo/users.html', context=user_dict)
+
 def users(request):
-	user_list = User.objects.order_by('first_name')
-	user_dict = {'users':user_list}
-	return render(request, 'appTwo/users.html', context=user_dict)
+	form = NewUserForm()
+
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print('ERROR FORM INVALID')
+
+
+	return render(request, 'appTwo/users.html', {'form': form})
